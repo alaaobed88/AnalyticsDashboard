e58d1181adc1a2cc036d5de4f53bf2d8c9c2a38b
Alaa Obeid <alaaobed88@gmail.com>
2023-08-06 03:39:54 +0300

    finished routes setup and nav bar component

--- AnalyticsDashboard/src/App.tsx
+++ AnalyticsDashboard/src/App.tsx
@@ -1,14 +1,29 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {createTheme} from "@mui/material/styles";
 import { themeSettings } from "./theme";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "@/scenes/navbar";
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              ></Route>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
