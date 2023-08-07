6e8ac37d5ec192dba007df9686437e783566b234
Alaa Obeid <alaaobed88@gmail.com>
2023-08-07 05:33:39 +0300

    finished redux setup and installation fixed some dependencies

--- AnalyticsDashboard/src/scenes/dashboard/Row1.tsx
+++ AnalyticsDashboard/src/scenes/dashboard/Row1.tsx
@@ -1,6 +1,9 @@
 import DashBoardBox from "@/components/DashBoardBox";
-
+import { useGetKpisQuery } from "@/state/api";
 const Row1 = () => {
+ 
+    const {data}=useGetKpisQuery();
+
   return (
     <>
       <DashBoardBox gridArea="a"></DashBoardBox>
