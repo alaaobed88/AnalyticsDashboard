ae75a4289fd0dc548c3b3891cbd580add49cd0c6
Alaa Obeid <alaaobed88@gmail.com>
2023-08-08 03:14:36 +0300

    setup mongo db completed fixed mongoose package

--- Server/index.js
+++ Server/index.js
@@ -6,15 +6,34 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-
 /*Configurations*/
 
 dotenv.config();
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/*MONGOOSE SETUP*/
+
+const MONGO_URL =
+  "mongodb+srv://alaaobed88:1234@cluster0.eoxwa5x.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the application with a non-zero error code
+  });
